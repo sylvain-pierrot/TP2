2aa456075b74e84bca14798116a965e5a5cb1e08
Petru Valicov <petru.valicov@univ-amu.fr>
2018-12-28 10:10:20 +0100

    creation projet Maven

---
+++ src/main/java/fr/umontpellier/iut/exercice1/FizzBuzz.java
@@ -0,0 +1,8 @@
+package fr.umontpellier.iut.exercice1;
+
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+        throw new RuntimeException("Not yet implemented !");
+    }
+}
