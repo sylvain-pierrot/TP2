07981da29c98c6a2a215a2dbc4b679858e300802
Petru Valicov <petru.valicov@univ-amu.fr>
2019-01-07 09:03:20 +0100

    correction FizzBuzz

--- src/main/java/fr/umontpellier/iut/exercice1/FizzBuzz.java
+++ src/main/java/fr/umontpellier/iut/exercice1/FizzBuzz.java
@@ -5,4 +5,12 @@ public class FizzBuzz {
     public static void main(String[] args) {
         throw new RuntimeException("Not yet implemented !");
     }
+
+    public String getValue(int i) {
+        throw new RuntimeException("Not yet implemented");
+    }
+
+    public String[] computeList(int i) {
+        throw new RuntimeException("Not yet implemented");
+    }
 }
